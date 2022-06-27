d1d6d74a9c9f5e24974f23f4320557a6fe129acc
Zahil Chuduang <zahil.c@ku.th>
2022-06-27 12:30:44 +0700

    add Main and update src/Dog.java

--- src/Dog.java
+++ src/Dog.java
@@ -1,5 +1,11 @@
 public class Dog {
-    public static void bark(){
-        System.out.println("woof woof!");
+    String name;
+    int age;
+    public Dog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public void bark(){
+        System.out.println(name + " : woof woof!");
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog noob = new Dog("noob", 12);
+        noob.bark();
+    }
+}
