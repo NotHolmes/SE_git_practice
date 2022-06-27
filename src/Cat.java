19563382a5c196870a86fd9f8d1fb932fd3e9741
Zahil Chuduang <zahil.c@ku.th>
2022-06-27 12:16:25 +0700

    Update Cat and add Dog

--- src/Cat.java
+++ src/Cat.java
@@ -1,5 +1,6 @@
 public class Cat {
     public static void main(String[] args) {
         System.out.println("Purrrrr~~~~~~~~~~~~~");
+        System.out.println("My name is Brownies!");
     }
 }
---
+++ src/Dog.java
@@ -0,0 +1,5 @@
+public class Dog {
+    public static void bark(){
+        System.out.println("woof woof!");
+    }
+}
