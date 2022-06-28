0abe4e337dff978093ccfa98551d353d6a1f21a9
NotHolmes <zahil.c@ku.th>
2022-06-28 01:31:52 +0000

    fix X.java

--- src/X.java
+++ src/X.java
@@ -1,5 +1,5 @@
 public class X {
-    public static call(){
-        System.out.println("This is X.");
+    public static void call(){
+        System.out.println("This is X from feature.");
     }
 }
